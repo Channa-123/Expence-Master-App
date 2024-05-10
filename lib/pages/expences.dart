0ed47e7b1b9bda6f2a36291b0beb1af8fbc9a8c0
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2024-05-11 01:23:42 +0530

    Create expences.dart

---
+++ lib/pages/expences.dart
@@ -0,0 +1,184 @@
+import 'package:expence_master/models/expence.dart';
+import 'package:expence_master/server/database.dart';
+import 'package:expence_master/widgects/Add_new_expence.dart';
+import 'package:expence_master/widgects/expence_list.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class Expences extends StatefulWidget {
+  const Expences({super.key});
+
+  @override
+  State<Expences> createState() => _ExpencesState();
+}
+
+class _ExpencesState extends State<Expences> {
+  final _myBox = Hive.box("expenceDatabase");
+  Database db = Database();
+
+  //expenceList
+  /*final List<ExpenceModel> _expenceList = [
+    ExpenceModel(
+        amount: 12.5,
+        date: DateTime.now(),
+        title: "Football",
+        category: Category.leasure),
+    ExpenceModel(
+        amount: 10,
+        date: DateTime.now(),
+        title: "Carrot",
+        category: Category.food),
+    ExpenceModel(
+        amount: 20,
+        date: DateTime.now(),
+        title: "Bag",
+        category: Category.travel),
+    ExpenceModel(
+        amount: 15,
+        date: DateTime.now(),
+        title: "Carrot",
+        category: Category.work),
+  ];*/
+
+  //pie chart
+  Map<String, double> dataMap = {
+    "Food": 0,
+    "Travel": 0,
+    "Leasure": 0,
+    "Work": 0,
+  };
+
+  //add new expence
+  void onAddNewExpence(ExpenceModel expence) {
+    setState(() {
+      db.expenceList.add(expence);
+      calCategoryValues();
+    });
+  }
+
+  //function to open a model overlay
+  void _openAddExpencesOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return AddNewExpence(
+          onAddExpence: onAddNewExpence,
+        );
+      },
+    );
+  }
+
+  //remove a expence
+  void onDeleteExpence(ExpenceModel expence) {
+    //store the deleting expence
+    ExpenceModel deletingExpence = expence;
+
+    //get the index of the removing expence
+    final int removingIndex = db.expenceList.indexOf(expence);
+    setState(() {
+      db.expenceList.remove(expence);
+      calCategoryValues();
+    });
+
+    //show snackbar
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: const Text("Delete Successfull"),
+        action: SnackBarAction(
+          label: "undo",
+          onPressed: () {
+            setState(() {
+              db.expenceList.insert(removingIndex, deletingExpence);
+              calCategoryValues();
+            });
+          },
+        )));
+  }
+
+  //pie chart
+  double foodVal = 0;
+  double travelVal = 0;
+  double leasureVal = 0;
+  double workVal = 0;
+
+  void calCategoryValues() {
+    double foodValTotal = 0;
+    double travelValTotal = 0;
+    double leasureValTotal = 0;
+    double workValTotal = 0;
+
+    for (final expence in db.expenceList) {
+      if (expence.category == Category.food) {
+        foodValTotal += expence.amount;
+      }
+      if (expence.category == Category.travel) {
+        travelValTotal += expence.amount;
+      }
+      if (expence.category == Category.leasure) {
+        leasureValTotal += expence.amount;
+      }
+      if (expence.category == Category.work) {
+        workValTotal += expence.amount;
+      }
+    }
+
+    setState(() {
+      foodVal = foodValTotal;
+      travelVal = travelValTotal;
+      leasureVal = leasureValTotal;
+      workVal = workValTotal;
+    });
+
+    //update the datamap
+    dataMap = {
+      "Food": foodVal,
+      "Travel": travelVal,
+      "Leasure": leasureVal,
+      "Work": workVal,
+    };
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    //if this is the first time create the initial data
+    if (_myBox.get("EXP_DATA") == null) {
+      db.createInitialDatabase();
+      calCategoryValues();
+    } else {
+      db.loadData();
+      calCategoryValues();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expence Master"),
+        backgroundColor: Color.fromARGB(255, 73, 163, 237),
+        elevation: 0,
+        actions: [
+          Container(
+            color: Colors.yellow,
+            child: IconButton(
+              onPressed: _openAddExpencesOverlay,
+              icon: const Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          PieChart(dataMap: dataMap),
+          ExpenceList(
+              expenceList: db.expenceList, onDeleteExpence: onDeleteExpence),
+        ],
+      ),
+    );
+  }
+}
